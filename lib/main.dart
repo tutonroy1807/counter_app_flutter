e14fa8ce27e64a3e0cc21d4b1334671f43fa3c6f
tutonroy1807 <prosenjitroy1807@gmail.com>
2023-07-21 13:10:56 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(Myapp());
+
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Counter App",
+      home: MyHomePage(),
+    );
+  }
+
+}
+
+class MyHomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageUI();
+  }
+
+}
+
+class MyHomePageUI extends State<MyHomePage>{
+  int countnumbers = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App-->"),),
+      body: Center(
+        child: Text(countnumbers.toString()),
+      ),
+      floatingActionButton: FloatingActionButton(
+
+        child: Icon(Icons.add),
+        onPressed: (){
+          setState(() {
+            countnumbers = countnumbers + 1;
+          });
+        },
+      ),
+    );
+  }
+
+}
